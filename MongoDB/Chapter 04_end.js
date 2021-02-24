3509332c0261f394dd85d9cdb940974be5efcb00
Suhong Li <sli@bryant.edu>
2021-02-23 19:16:56 -0500

    added chapter 4 end file

--- MongoDB/Chapter 04_end.js
+++ MongoDB/Chapter 04_end.js
@@ -68,7 +68,7 @@ db.movies.find(
 //8. Find all unrated movies in 2008.
 db.movies.find(
     {$and:[
-        {"rated": "UNRATED"},
+        {"rated": "Unrated"},
         {"year": 2008}
     ]},
     {
@@ -79,6 +79,18 @@ db.movies.find(
     }
 )
 
+//implicit and
+
+db.movies.find(
+    {"rated": "UNRATED", 
+     "year": 2008},
+    {
+        "title":1,
+        "rated":1,
+        "year":1,
+        "_id":0
+    }
+)
 
 // logical operators $and, $or, $nor, $not
 //9. find all movies where Leonardo DiCaprio is a cast member, Martin Scorsese is a director, is in either Drama or Crime genres.
@@ -104,7 +116,25 @@ db.movies.find(
     }
 )
 
+//implict and
 
+db.movies.find(
+    {
+      "cast":"Leonardo DiCaprio",
+      "directors": "Martin Scorsese",
+       $or:[
+                {"genres": "Drama"},
+                {"genres": "Crime"}
+            ]
+     },     
+    {
+        "title":1,
+        "cast":1,
+        "directors":1,
+        "genres":1,
+        "_id":0
+    }
+)
 //Regular Expressions. It is case sensitive
 //10. Find all movies where title includes Opera
 
@@ -166,60 +196,144 @@ db.movies.find(
 
 //12. find all movies starting with number and include word Street
 
-
+db.movies.find(
+    {$and:[
+        {"title": {$regex:"^[0-9]"}},
+        {"title": {$regex:"Street"}}
+    ]},
+    {
+        "title":1,
+        "_id":0
+    }
+)
 
 
 //query arrays and nested documents
 //finding an array by an element
 //13. find all movies where Charles Chaplin and Edna Purviance are cast members.
-
-
-
+db.movies.find({
+    "cast": {$all:
+        ["Charles Chaplin", "Edna Purviance"]}
+},
+{
+    "cast":1,
+    "_id":0
+}
+)
 
 
 //Projecting Array Elements using $
 // 14. find all movies where genre is Drama.
 
-
+db.movies.find(
+    {
+    "genres": "Drama"},
+    {
+        "genres":1,
+        "title":1,
+        "_id":0
+    }
+)
 
 
 //display the first matching element.
 
+db.movies.find(
+    {
+    "genres": "Drama"},
+    {
+        "genres.$":1,
+        "title":1,
+        "_id":0
+    }
+)
 
 
 //Projecting matching elements by their index position ($slice)
 //15. display movie titled Youth without youth
+db.movies.find(
+    {
+        "title": "Youth Without Youth"
+    },
+    {
+        "genres":1,
+        "title":1,
+        "_id":0
+    }
+)
 
 
 
+//return the first two elements
 
-//return the first three elements
-
-
+db.movies.find(
+    {
+        "title": "Youth Without Youth"
+    },
+    {
+        "genres":{$slice:2},
+        "title":1,
+        "_id":0
+    }
+)
 
 
 //return the last two elements only
 
-
+db.movies.find(
+    {
+        "title": "Youth Without Youth"
+    },
+    {
+        "genres":{$slice:-2},
+        "title":1,
+        "_id":0
+    }
+)
 
 
 //starting at second element and pick 2
 
-
-
+db.movies.find(
+    {
+        "title": "Youth Without Youth"
+    },
+    {
+        "genres":{$slice:[1,2]},
+        "title":1,
+        "_id":0
+    }
+)
 
 //Query nested objects
 // sorting documents. 1: ascending, -1: descending
 // 16. Find movies that have won at least 10 awards and received 6 nominations. Display awards and title. Only show top 5.
-
+db.movies.find(
+    {
+        "awards.wins": {$gt:10},
+        "awards.nominations":6
+    },
+    {
+        "title":1,
+        "awards":1,
+        "_id":0
+    }
+).sort({"awards.wins":-1}).limit(5).pretty()
 
 
 
 //17. Display all movies where Charles Chaplin is a cast memeber. Display title, awards win and genres. 
 //sort by the first element in genre and then awards win.
 
-
-
+db.movies.find({
+    "cast": "Charles Chaplin"
+}, {
+    "title":1,
+    "awards.wins":1,
+    "genres":1,
+    "_id":0
+}
+).sort({"genres.0":1, "awards.wins":-1})
 
 
 //Activity 4.0.1
@@ -231,14 +345,14 @@ var pageSize = 5
 
 function findMoviesByGenre(genre, pageSize){
     
-    var movies = db.movies.find(
+    var result = db.movies.find(
         {"genres" : genre},
         {"_id" : 0, "title" :1, "genres":1}).
         sort({"imdb.rating" : -1}).
         limit(pageSize).
         toArray()
 
-    return movies
+    return result
 }
 
 //call function
